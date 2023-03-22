61f1f4468694307dacb61fa195cda859c376ad39
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-03-22 10:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
+	let sum=Number.MAX_VALUE;
+	for(let i=0;i<arr.length;i++){
+		for(let j=i+1;j<arr.length;j++){
+			for(let k=j+1;k<arr.length;k++){
+				let s=arr[i]+arr[j]+arr[k];
+				s=Math.abs(s);
+				if(s<sum && s>=target){
+					sum=s;
+				}
+			}
+		}
+	}
+	return sum;
   
 }
 
